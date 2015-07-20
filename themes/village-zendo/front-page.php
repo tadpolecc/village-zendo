a45130c3c40584b8a9a24f418988fdc8b55e5173
Rindy Portfolio <rindy@tadpole.cc>
2015-07-19 23:34:49 -0400

    Remove pagination from home page

--- themes/village-zendo/front-page.php
+++ themes/village-zendo/front-page.php
@@ -28,6 +28,7 @@ function tc_home_loop() {
     remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
     remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
     remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
+    remove_action( 'genesis_after_endwhile', 'genesis_posts_nav' );
 ?>
  	
  	<h2>Features</h2>
